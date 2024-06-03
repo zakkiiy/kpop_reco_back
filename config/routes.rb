6b46dc9f662391c124430030a2925b2d3acd59d5
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-06-03 21:21:57 +0900

    kpop_video#indexの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   post "auth/:provider/callback", to: "api/v1/users#create"
   namespace :api do
     namespace :v1 do
+      resources :kpop_videos
       get 'youtube_video_api_tests/latest', to: 'youtube_video_api_tests#latest_videos'
       post 'kpop_videos/create_videos', to: 'kpop_videos#create_videos' 
     end
