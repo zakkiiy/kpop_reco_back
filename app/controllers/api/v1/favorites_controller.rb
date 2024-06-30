1e583060794a114ab79bfef240f9a6d86b162df3
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-06-30 20:12:46 +0900

    お気に入りの削除実装

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -15,4 +15,27 @@ class Api::V1::FavoritesController < ApplicationController
       render json: { success: false, errors: e.message }, status: :internal_server_error
     end
   end
+
+  def destroy
+    video = KpopVideo.find(params[:video_id])
+    favorite = @current_user.favorites.find_by(kpop_video_id: video)
+    if favorite
+      favorite.destroy
+      render json: { success: true }, status: :ok
+    else
+      render json: { success: false, errors: ['Favorite not found'] }, status: :not_found
+    end
+  end
+
+  def check
+    video = KpopVideo.find(params[:id])
+    favorite = @current_user.favorites.find_by(kpop_video_id: video)
+    render json: { isFavorite: favorite.present? }
+  end
+
+  private
+
+  def favorite_params
+    params.require(:favorite).permit(:kpop_video_id)
+  end
 end
