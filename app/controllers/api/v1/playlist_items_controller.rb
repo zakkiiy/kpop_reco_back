e50788e24d8c6e511bc92820a31cd393de43c473
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-07-13 00:17:41 +0900

    playlist_items#destroyの実装

--- app/controllers/api/v1/playlist_items_controller.rb
+++ app/controllers/api/v1/playlist_items_controller.rb
@@ -1,24 +1,39 @@
 class Api::V1::PlaylistItemsController < ApplicationController
   before_action :set_current_user
+  before_action :set_playlist
 
   def index
-    playlist = @current_user.playlists.find(params[:playlist_id])
-    playlist_items = playlist.playlist_items
+    playlist_items = @playlist.playlist_items.includes(:kpop_video)
     render json: {
-      playlist_id: playlist.id,
-      playlist_name: playlist.name,
-      items: playlist_items.as_json(include: { kpop_video: { only: [:id, :title] } })
+      playlist_id: @playlist.id,
+      playlist_name: @playlist.name,
+      items: playlist_items.as_json(include: { kpop_video: { only: [:id, :name] } })
     }, status: :ok
   end
   
   def create
-    playlist = @current_user.playlists.find(params[:playlist_id])
-    playlist_item = playlist.playlist_items.build(kpop_video_id: params[:kpop_video_id])
+    playlist_item = @playlist.playlist_items.build(kpop_video_id: params[:kpop_video_id])
 
     if playlist_item.save
       render json: { message: 'Playlist item created successfully' }, status: :created
     else
-      render json: { errors: playlist_item.errors.full_messages }, status: :unprocessable_entity
+      render json: { error: 'プレイリストアイテムの作成に失敗しました' }, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    playlist_item = @playlist.playlist_items.find_by(kpop_video_id: params[:id])
+    if playlist_item
+      playlist_item.destroy
+      head :no_content
+    else
+      render json: { error: 'プレイリストアイテムが見つかりません' }, status: :not_found
+    end
+  end
+
+  private
+
+  def set_playlist
+    @playlist = @current_user.playlists.find(params[:playlist_id])
   end
 end
